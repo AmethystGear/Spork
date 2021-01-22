9d266fae835e3acd9fe5307f9f6bf868e3a03d4a
AmethystGear <shivenbhatt@outlook.com>
2021-01-22 14:39:26 -0800

    added string literals and improved parsing

--- charstream.py
+++ charstream.py
@@ -1,10 +1,16 @@
+import copy 
+
+class CharStreamState:
+    def __init__(self, pos, line, col):
+        self.pos = pos
+        self.line = line
+        self.col = col
+
 # makes the whole program into a character stream 
 # so we can get each character one at a time
 class CharStream:
     def __init__(self, program: str):
-        self.pos = 0
-        self.line = 1
-        self.col = 0
+        self.stream_state = CharStreamState(0, 1, 0)
         self.program = program
 
     # return the current character and advance to the next character
@@ -14,30 +20,30 @@ class CharStream:
             raise Exception('Cannot call next() when there is nothing left to read in the program')
 
         curr = self.peek()
-        self.pos += 1
+        self.stream_state.pos += 1
         if curr == '\n':
-            self.line += 1
-            self.col = 0
+            self.stream_state.line += 1
+            self.stream_state.col = 0
         else:
-            self.col += 1
+            self.stream_state.col += 1
         
         return curr
 
     # get the current character, or None if there aren't any characters left in the program
     def peek(self) -> str:
-        return None if self.eof() else self.program[self.pos]
+        return None if self.eof() else self.program[self.stream_state.pos]
 
     # return true if we've reached the end of the program, else false
     def eof(self) -> bool:
-        return self.pos >= len(self.program)
+        return self.stream_state.pos >= len(self.program)
 
     # fail program compilation at our current position, with a message
     def err(self, msg: str):
-        print("Compilation failed at line: " + str(self.line) + ", col: " + str(self.col))
+        print("Compilation failed at line: " + str(self.stream_state.line) + ", col: " + str(self.stream_state.col))
         print(msg)
-        col = self.col
-        self.pos -= self.col
-        self.col = 0
+        col = self.stream_state.col
+        self.stream_state.pos -= self.stream_state.col
+        self.stream_state.col = 0
         while self.peek() != '\n':
             print(self.next(), end='')
 
@@ -49,3 +55,8 @@ class CharStream:
 
         quit()
 
+    def get_state(self) -> CharStreamState:
+        return copy.deepcopy(self.stream_state)
+
+    def reset_to(self, state: CharStreamState):
+        self.stream_state = copy.deepcopy(state)
--- lexer.py
+++ lexer.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from enum import Enum, auto
 from functools import partial
 from charstream import CharStream
@@ -16,15 +17,10 @@ class TokenType(Enum):
 # if a match is not found, returns None and resets the CharStream 
 # as if the fn was never run.
 def match(token_type, fn, char_stream: CharStream):
-    pos = char_stream.pos
-    col = char_stream.col
-    line = char_stream.line
-
+    state = char_stream.get_state()
     matching = fn(char_stream)
     if matching is None:
-        char_stream.pos = pos
-        char_stream.col = col
-        char_stream.line = line
+        char_stream.reset_to(state)
         return None
     else:
         return Token(token_type, matching)
@@ -42,36 +38,91 @@ class Token():
 # defining the functions we use to recognize tokens #
 #####################################################
 
-# match a 
-def exact(val: str, require_whitespace_after: bool, char_stream: CharStream):
+# match an exact sequence of characters
+def parse_exact(val: str, char_stream: CharStream, valid_post_tokens=[], require_whitespace=False):
     val_stream = CharStream(val)
-    while val_stream.peek() == char_stream.peek():
+    while not val_stream.eof() and not char_stream.eof() and val_stream.peek() == char_stream.peek():
         val_stream.next()
         char_stream.next()
 
-    if require_whitespace_after and not char_stream.peek().isspace():
+    if not val_stream.eof():
         return None
+    elif char_stream.eof() or char_stream.peek().isspace():
+        return val
+    elif valid_post_tokens:
+        found_valid_token = False
+        state = char_stream.get_state()
+        for token in valid_post_tokens:
+            if token(char_stream) is not None:
+                found_valid_token = True
+                break
 
-    return val if val_stream.eof() else None
+        char_stream.reset_to(state)
+        return val if found_valid_token else None
+    else:
+        return None if require_whitespace else val
 
-def number(char_stream: CharStream):
+# match a number literal
+def parse_number(valid_post_tokens, char_stream: CharStream):
     num = ''
-    if char_stream.peek() == '-':
-        num += '-'
-
     while not char_stream.eof() and char_stream.peek().isdigit():
         num += char_stream.next()
 
-    return None if num == '' or num == '-' else num
+    found_valid_token = False
+    state = char_stream.get_state()
+    for token in valid_post_tokens:
+        if token(char_stream) is not None:
+            found_valid_token = True
+            break
+    
+    char_stream.reset_to(state)
 
-def identifier(keywords, char_stream: CharStream):
+    return None if num == '' or (not found_valid_token and valid_post_tokens) else num
+
+# match a boolean literal
+def parse_boolean(valid_post_tokens, char_stream: CharStream):
+    if char_stream.peek() == 't':
+        return parse_exact('true', char_stream, valid_post_tokens=valid_post_tokens)
+    elif char_stream.peek() == 'f':
+        return parse_exact('false', char_stream, valid_post_tokens=valid_post_tokens)
+    else:
+        return None
+
+# match a string literal
+def parse_string(char_stream: CharStream):
+    string = ''
+    if char_stream.peek() != '"':
+        return None
+    
+    string += char_stream.next()
+    escape = False
+    while not char_stream.eof():
+        char = char_stream.next()
+        string += char
+        if char == '\\':
+            escape = not escape
+        elif char == '"' and not escape:
+            break
+        else:
+            escape = False
+
+    return string if string[0] == '"' and string[-1] == '"' else None
+        
+# match an identifier
+def parse_identifier(banned_tokens, char_stream: CharStream):
     ident = ''
+    if char_stream.peek().isdigit():
+        return None
+
     while not char_stream.eof() and (char_stream.peek().isalnum() or char_stream.peek() == '_'):
         ident += char_stream.next()
 
-    for kw in keywords:
-        if (kw(CharStream(ident + ' '))):
+    state = char_stream.get_state()
+    for banned in banned_tokens:      
+        if (banned(CharStream(ident))):
             return None
+        
+        char_stream.reset_to(state)
 
     return None if ident == '' else ident
 
@@ -82,30 +133,37 @@ def identifier(keywords, char_stream: CharStream):
 ##########################################
 
 # punctuation
-left_paren = partial(match, TokenType.PUNC, partial(exact, '(', False))
-right_paren = partial(match, TokenType.PUNC, partial(exact, ')', False))
-left_bracket = partial(match, TokenType.PUNC, partial(exact, '{', False))
-right_bracket = partial(match, TokenType.PUNC, partial(exact, '}', False))
-delim = partial(match, TokenType.PUNC, partial(exact, ',', False))
-terminator = partial(match, TokenType.PUNC, partial(exact, ';', False))
-type_specifier = partial(match, TokenType.PUNC, partial(exact, ':', False))
+left_paren = partial(match, TokenType.PUNC, partial(parse_exact, '('))
+right_paren = partial(match, TokenType.PUNC, partial(parse_exact, ')'))
+left_bracket = partial(match, TokenType.PUNC, partial(parse_exact, '{'))
+right_bracket = partial(match, TokenType.PUNC, partial(parse_exact, '}'))
+delim = partial(match, TokenType.PUNC, partial(parse_exact, ','))
+terminator = partial(match, TokenType.PUNC, partial(parse_exact, ';'))
+type_specifier = partial(match, TokenType.PUNC, partial(parse_exact, ':'))
 punctuation = [left_paren, right_paren, left_bracket, right_bracket, delim, terminator, type_specifier]
 
 # operators
-eq = partial(match, TokenType.OPERATOR, partial(exact, '=', False))
-into = partial(match, TokenType.OPERATOR, partial(exact, '->', False))
-ops = [eq, into]
+eq = partial(match, TokenType.OPERATOR, partial(parse_exact, '='))
+into = partial(match, TokenType.OPERATOR, partial(parse_exact, '->'))
+add = partial(match, TokenType.OPERATOR, partial(parse_exact, '+'))
+sub = partial(match, TokenType.OPERATOR, partial(parse_exact, '-'))
+power = partial(match, TokenType.OPERATOR, partial(parse_exact, '**'))
+mul = partial(match, TokenType.OPERATOR, partial(parse_exact, '*'))
+div = partial(match, TokenType.OPERATOR, partial(parse_exact, '/'))
+ops = [eq, into, add, sub, power, mul, div]
 
 # keywords 
-let = partial(match, TokenType.KEYWORD, partial(exact, 'let', True))
+let = partial(match, TokenType.KEYWORD, partial(parse_exact, 'let', require_whitespace=True))
 keywords = [let]
 
 # literals
-num = partial(match, TokenType.LIT, number)
-literals = [num]
+num = partial(match, TokenType.LIT, partial(parse_number, punctuation + ops))
+boolean = partial(match, TokenType.LIT, partial(parse_boolean, punctuation + ops))
+string = partial(match, TokenType.LIT, parse_string)
+literals = [num, boolean, string]
 
 # identifiers
-ident = partial(match, TokenType.IDENT, partial(identifier, keywords))
+ident = partial(match, TokenType.IDENT, partial(parse_identifier, keywords + literals))
 
 ##############################################
 
@@ -118,6 +176,7 @@ class TokenStream:
         self.char_stream = char_stream
         self.current = None
 
+    # read in the next token and return it
     def next(self) -> Token:
         # handle eof case
         if self.eof():
@@ -140,7 +199,7 @@ class TokenStream:
         
         token = None
         for token_type in token_types:
-            token = token_type(self.char_stream)
+            token = token_type(char_stream=self.char_stream)
             if token is not None:
                 break
 
@@ -154,13 +213,15 @@ class TokenStream:
         self.current = token
         return token
 
-
+    # return the current token
     def peek(self) -> Token:
         return current
 
+    # return if we're at the end of the file or not
     def eof(self) -> bool:
         return self.char_stream.eof()
         
+# testing this for 
 if __name__ == "__main__":
     import sys
     file = sys.argv[1]
