1b8c0e61708682ec313abcb229c503f24a23b8b6
Shiven Bhatt <ShivenBhatt@outlook.com>
2021-02-22 23:06:58 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-ocamlopt -o embr_interpreter files.ml lexer.ml parser.ml interpreter.ml
-./embr_interpreter "$1"
-rm embr_interpreter
+ocamlopt -o spork_interpreter files.ml lexer.ml parser.ml interpreter.ml
+./spork_interpreter "$1"
+rm spork_interpreter
